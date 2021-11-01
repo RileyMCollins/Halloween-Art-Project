a90f0576abd34b5a604666bb6a85bd79ffffe160
RileyMCollins <92051463+RileyMCollins@users.noreply.github.com>
2021-11-01 10:56:16 -0700

    Add files via upload

---
+++ HalloweenArt.py
@@ -0,0 +1,35 @@
+#-----import statements-----
+import turtle as trtl
+import random as rand
+t = trtl.Turtle()
+t.shapesize(5)
+t.fillcolor("white")
+xpos = 0
+ypos = 0
+t.penup()
+wn = trtl.Screen()
+wn.bgpic("universe.gif")
+wn.addshape("ezgif.com-gif-maker3.gif")
+t.shape("ezgif.com-gif-maker3.gif")
+
+
+
+
+
+def change_pos():
+  t.forward(100)
+  num1 = rand.randint(1, 360)
+  t.left(num1)
+
+def click(x, y):
+  change_pos()
+
+
+
+if t.onclick(click):
+  change_pos()
+
+
+
+
+wn.mainloop()
